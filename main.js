5fa7baf8999d0437f6ffff79324c04bfe55d6475
mwisechacha <angieschacha@gmail.com>
2022-12-25 18:33:14 +0300

    fixed styles css

--- main.js
+++ main.js
@@ -3,88 +3,85 @@ const confirmButton = document.getElementById("confirm-button");
 const detailsSection = document.querySelector(".fill-details");
 const confirmSection = document.querySelector(".complete-section");
 
+const form = document.querySelector("form");
+
 const holderName = document.getElementById("holder-name");
 const cardNumber = document.getElementById("card-num");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
 const cvcNumber = document.getElementById("cvc");
 
-confirmButton.addEventListener("submit", (e) => {
+const nameOfHolder = document.getElementById("name");
+function cardName(e) {
+  nameOfHolder.innerText = e.target.value;
+}
+
+const number = document.getElementById("numbers");
+function cardNumberInput(e) {
+  number.innerText = format(e.target.value);
+}
+function format(s) {
+  return s.toString().replace(/\d{4}(?=.)/g, "$&");
+}
+
+const monthDate = document.getElementById("month-date");
+function cardMonth(e) {
+  monthDate.innerText = e.target.value;
+}
+
+const yearDate = document.getElementById("year-date");
+function cardYear(e) {
+  yearDate.innerText = e.target.value;
+}
+
+const cvcCard = document.getElementById("cvc-number");
+function cardCvc(e) {
+  cvcCard.innerText = e.target.value;
+}
+function format(s) {
+  return s.toString().replace(/\d{4}(?=.)/g, "$&");
+}
+
+holderName.addEventListener("keyup", cardName);
+cardNumber.addEventListener("keyup", cardNumberInput);
+month.addEventListener("keyup", cardMonth);
+year.addEventListener("keyup", cardYear);
+cvcNumber.addEventListener("keyup", cardCvc);
+
+function submitDetails(e) {
   e.preventDefault();
-
-  detailsSection.classList.add("hidden");
-  confirmSection.style.display = "initial";
-  validation();
-});
-
-const err = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = message;
-  inputControl.classList.add("error");
-  inputControl.classList.remove("success");
-};
-
-const succ = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = "";
-  inputControl.classList.add("success");
-  inputControl.classList.remove("error");
-};
-
-const validation = () => {
-  const holderValue = holderName.value.trim();
-  const cardValue = cardNumber.value.trim();
-  const monthNumber = month.value.trim();
-  const yearNumber = year.value.trim();
-  const cvc = cvcNumber.value.trim();
-
-  if (holderValue === "") {
-    err(holderName, "CardHolder Name is required");
+  const numberError = document.getElementById("card-number-error");
+  if (cardNumber.value.length > 0 && cardNumber.value.length < 16) {
+    numberError.innerHTML = "Wrong Format";
+  } else if ((cardNumber.value = "")) {
+    numberError.innerHTML = "Can't be blank";
   } else {
-    succ(holderName);
+    numberError.innerHTML = "";
+  }
+  const expiryError = document.getElementById("date-expiry-error");
+  if (month.value && year.value == "") {
+    expiryError.innerHTML = "Can't be blank";
+  } else {
+    expiryError.innerHTML = "";
+  }
+  const cvcError = document.getElementById("cvc-error");
+  if (cvcNumber.value == "") {
+    cvcError.innerHTML = "Can't be blank";
+  } else {
+    cvcError.innerHTML = "";
   }
 
-  if (cardValue === "") {
-    err(cardNumber, "Card number is required");
-  } else if (cardValue.isInteger() == false) {
-    err(cardNumber, "Wrong format,numbers only");
-  } else if (cardValue.length != 16) {
-    err(cardNumber, "Enter correct number");
-  } else {
-    succ(cardNumber);
+  if (
+    holderName.value &&
+    cardNumber.value &&
+    month.value &&
+    year.value &&
+    cvcNumber &&
+    cardNumber.value.length == 16
+  ) {
+    confirmSection.classList.remove("hidden");
+    detailsSection.classList.add("hidden");
   }
+}
 
-  if (monthNumber === "") {
-    err(month, "Can't be blank");
-  } else if (monthNumber.isInteger() == false) {
-    err(month, "Wrong format,numbers only");
-  } else if (month.length != 2) {
-    err(month, "Enter correct number");
-  } else {
-    succ(month);
-  }
-
-  if (yearNumber === "") {
-    err(year, "Can't be blank");
-  } else if (yearNumber.isInteger() == false) {
-    err(year, "Wrong format,numbers only");
-  } else if (yearNumber.length != 2) {
-    err(year, "Enter correct number");
-  } else {
-    succ(year);
-  }
-
-  if (cvc === "") {
-    err(cvcNumber, "can't be blank");
-  } else if (cvc.isInteger() == false) {
-    err(cvc, "Wrong format,numbers only");
-  } else if (cvc.length != 3) {
-    err(cvcNumber, "Enter correct number");
-  } else {
-    succ(cvcNumber);
-  }
-};
+confirmButton.addEventListener("click", submitDetails);
